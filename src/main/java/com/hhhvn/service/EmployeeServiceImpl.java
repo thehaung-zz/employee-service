a2279beed0b7df6ca566fcc0d96906d50202801a
Vuong <vuong@11.com>
2021-11-23 23:47:35 +0700

    fix employeeId lan2

--- src/main/java/com/hhhvn/service/EmployeeServiceImpl.java
+++ src/main/java/com/hhhvn/service/EmployeeServiceImpl.java
@@ -4,12 +4,16 @@ import com.hhhvn.VO.Department;
 import com.hhhvn.VO.ResponseTemplateVO;
 import com.hhhvn.enntity.Employee;
 import com.hhhvn.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
     @Autowired
@@ -20,10 +24,12 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     @Override
     public Employee saveEmployee(Employee employee){
+        log.info("Inside save");
         return employeeRepository.save((employee));
     }
     @Override
     public List<Employee> getAllEmployee() {
+        log.info("Inside getAll");
         List<Employee> list= employeeRepository.findAll();
         return list;
     }
@@ -38,29 +44,35 @@ public class EmployeeServiceImpl implements EmployeeService{
         return vo;
     }
     @Override
-    public String deleteEmployee(Long id) {
-        Employee employee = employeeRepository.findById(id).get();
-        if(employee!=null){
-            employeeRepository.delete(employee);
-            return "Delete Sucessful";
-        }else{
-            return "Delete fail";
+    public HttpStatus deleteEmployee(Long id) {
+        log.info("Inside delete");
+        try {
+            Employee employee = employeeRepository.findById(id).get();
+            return HttpStatus.OK;
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "NOT FOUND");
         }
+
     }
+
 
     @Override
-    public Employee updateEmployeeId(Long id, Employee employeeIdd) {
-        Employee employee = employeeRepository.findById(id).get();
-        if(employee==null) {
-            return null;
-        }else {
-            employee.setFirstName(employeeIdd.getFirstName());
-            employee.setLastName(employeeIdd.getLastName());
-            employee.setEmail(employeeIdd.getEmail());
-            return employeeRepository.save(employee);
-        }
-    }
+    public Employee updateEmployeeId(Long id, Employee employeeData) {
+        log.info("Inside update");
 
+        try{
+        Employee employee = employeeRepository.findById(id).get();
+            employee.setFirstName(employeeData.getFirstName());
+            employee.setLastName(employeeData.getLastName());
+            employee.setEmail(employeeData.getEmail());
+            return employeeRepository.save(employee);
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "NOT FOUND");
+        }
+
+    }
 
 }
 
