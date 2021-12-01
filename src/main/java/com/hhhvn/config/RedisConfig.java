6eefb462b7c9cccde66570a81927008dbd63fb3f
Hau Nguyen <thehaung217@gmail.com>
2021-12-01 23:43:20 +0700

    build: add config redis

---
+++ src/main/java/com/hhhvn/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.hhhvn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @Author : Hau Nguyen
+ * @Created : 12/1/21, Wednesday
+ **/
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Tạo Standalone Connection tới Redis
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("172.17.0.2", 6379));
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
